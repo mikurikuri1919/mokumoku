13c0f5b378d7a6a0a87e2fffd28bb6bd0ec8af38
mikurikuri1919 <mikuriyakenta003@gmail.com>
2023-09-15 23:10:01 +0900

    Fix: Handle potential nil value in unfollow method

--- app/models/user.rb
+++ app/models/user.rb
@@ -88,7 +88,8 @@ class User < ApplicationRecord
   end
 
   def unfollow(user_id)
-    relationships.find_by(followed_id: user_id).destroy
+    relationship = relationships.find_by(followed_id: user_id)
+    relationship&.destroy
   end
 
   def following?(user)
